d40e4aca7d6e364befaa3698bd9bb24fc8e56753
smultron <fredholmisak@outlook.com>
2024-08-19 21:20:37 +0200

    added random announcer voice

--- gmtk2024/Assets/Runtime/Audio/AnnouncerSoundController.cs
+++ gmtk2024/Assets/Runtime/Audio/AnnouncerSoundController.cs
@@ -1,5 +1,4 @@
 using FMODUnity;
-using UnityEngine;
 
 public class AnnouncerSoundController : MonoSingleton<AnnouncerSoundController> 
 {
@@ -9,11 +8,27 @@ public class AnnouncerSoundController : MonoSingleton<AnnouncerSoundController>
     public EventReference RandomCommentaryRef;
     public EventReference CheckpointRef;
 
+    private float _CommentaryTimer;
+    public float commentaryInterval = 10f; // Interval between random commentary, in seconds
+
     void Start() {
         FMODUtil.PlayOneShot(GameIntroRef);
         GameManager.Instance.Player.HealthZero += () => FMODUtil.PlayOneShot(GameEndRef);
-
+        _CommentaryTimer = commentaryInterval;
     }
-    
 
+    void Update() {
+        if (GameManager.Instance.GameState == GameState.Platforming) {
+            _CommentaryTimer -= Time.deltaTime;
+
+            if (_CommentaryTimer <= 0f) {
+                PlayRandomCommentary();
+                _CommentaryTimer = commentaryInterval;
+            }
+        }
+    }
+
+    private void PlayRandomCommentary() {
+        FMODUtil.PlayOneShot(RandomCommentaryRef);
+    }
 }
